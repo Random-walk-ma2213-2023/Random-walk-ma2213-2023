c61a9896ed1ea8749fe2027d6364ac18ee76eeae
Random-walk-ma2213-2023 <134778690+Random-walk-ma2213-2023@users.noreply.github.com>
2023-05-27 18:05:55 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -102,7 +102,7 @@ function draw() {
   cust();
 
   fill("#B8621B");
-  rect(width / 2 - 100, height - 175, width / 2 - 50, height / 4 - 130);
+  rect(width / 2 - 100, 520, width / 2 - 50, 43.75);
   textAlign(LEFT);
   fill("white");
   text("Rata-rata jarak tempuh: ", width / 2 - 85, height - 150);
@@ -162,7 +162,7 @@ function Member() {
 
 function cust() {
   fill("#B8621B");
-  rect(width / 4 - 300, height / 2 - 200, width / 4 + 50, height - 275);
+  rect(50, 150, width / 4 + 50, 420);
 
   a = width / 4;
   b = height / 2;
